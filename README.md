c54d3c06d93423644f5800e2f92e6b2da45cb5eb
Cyprien023 <cyprienluzuy@gmail.com>
2024-01-12 16:11:57 +0100

    ajout pagination + début form et authentification

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Symfony-project
 Application web de gestion de contenus en PHP/Symfony
+Wiki Clash Of Clans
--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -6,25 +6,40 @@ use App\Entity\Content;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 class ContentController extends AbstractController
 {
     #[Route('/contenu/liste', name: 'content_list')]
 
-    public function list(EntityManagerInterface $entityManager, LoggerInterface $logger): Response
-    {
+    public function list(
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
         $contenus = $entityManager->getRepository(Content::class)->findAll();
 
         if (!$contenus) {
             throw $this->createNotFoundException('Content not found');
         }
 
-        return $this->render('content/liste.html.twig', ['contenus' => $contenus]);
+        $pagination = $paginator->paginate(
+            $contenus,
+            $request->query->getInt('page', 1),
+            1
+        );
+
+        return $this->render('content/liste.html.twig', ['contenus' => $pagination]);
     }
+
+    #[Route('/contenu/{id}', name: 'content_show')]
+    public function show(Content $contenu): Response
+    {
+        return $this->render('content/show.html.twig', ['contenu' => $contenu]);
+    }
+
 }
-
-
-
